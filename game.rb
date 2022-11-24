c4128bed17dc9119c487825b77dcb356975b85e6
nathancabigao <nathan0714@hotmail.com>
2022-11-24 14:14:31 -0500

    Add and use constants for col/row

--- game.rb
+++ game.rb
@@ -1,3 +1,11 @@
+# frozen_string_literal: true
+
+# Constants for rows and columns
+TOP = LEFT = 0
+MIDDLE = 1
+BOTTOM = RIGHT = 2
+
+# Used to create 3x3 grid boards for the game.
 class Board
   def initialize
     @board_grid = Array.new(3) { Array.new(3) } # 3 rows of [nil, nil, nil]
@@ -9,7 +17,7 @@ class Board
 
   def place_marker(marker, row, column)
     # if there is no marker at @board_grid[row][column], set the marker there
-    return 'ERROR: Invalid row/column' unless row.between?(0, 2) && column.between?(0, 2)
+    return 'ERROR: Invalid row/column' unless row.between?(TOP, BOTTOM) && column.between?(LEFT, RIGHT)
     return 'You cannot place a marker here.' unless @board_grid[row][column].nil?
 
     @board_grid[row][column] = marker
